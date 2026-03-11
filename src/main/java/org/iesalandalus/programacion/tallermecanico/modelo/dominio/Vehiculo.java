a50b466b4bbba660dbb923929dc45e900a60c458
tmainara <ainaratorresmagan@gmail.com>
2026-03-11 09:47:03 +0100

    He hecho la clase vehiculo

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Vehiculo.java
@@ -0,0 +1,52 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
+
+import java.util.Objects;
+
+public record Vehiculo(String marca, String modelo, String matricula) {
+    private static final String ER_MARCA = "[A-Z][a-z]+(?:[- ]?[A-Z]+)?|[A-Z]+";
+    private static final String ER_MATRICULA = "\\d{4}[^\\W_AEIOUa-z]{3}" ;
+
+    public Vehiculo{
+        validarMarca(marca);
+        validarMatricula(matricula);
+        validarModelo(modelo);
+    }
+    private void validarMarca (String marca){
+        Objects.requireNonNull(marca, "La marca no puede ser nula.");
+        if (!marca.matches(ER_MARCA)){
+            throw new IllegalArgumentException("El formato de la marca no es válido.");
+        }
+    }
+    private void validarModelo (String modelo){
+        Objects.requireNonNull(modelo,"El modelo no puede ser nulo.");
+        if (modelo.isBlank()){
+            throw new IllegalArgumentException("El modelo no puede estar en blanco");
+        }
+    }
+    private void  validarMatricula (String matricula){
+        Objects.requireNonNull(matricula,"La matrícula no puede ser nula.");
+        if (!matricula.matches(ER_MATRICULA)){
+            throw new IllegalArgumentException("El formato de la matrícula no es válido.");
+        }
+    }
+    public static Vehiculo get ( String matricula){
+        return new Vehiculo("Seat","León", matricula);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Vehiculo vehiculo = (Vehiculo) o;
+        return Objects.equals(matricula, vehiculo.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matricula);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s - %s", marca,modelo, matricula);
+    }
+}
